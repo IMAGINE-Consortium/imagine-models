2544f158f918e291c587989f4a05f13fc59a3030
shutsch <hutsch@astro.ru.nl>
2024-02-13 10:57:32 +0100

    debugged test_unifrom, introduced more matchers

--- c_library/test/regular/test_uniform.cc
+++ c_library/test/regular/test_uniform.cc
@@ -1,9 +1,11 @@
 #include <catch2/catch_test_macros.hpp>
+#include <catch2/catch_all.hpp>
 #include <cassert>
 #include <iostream>
 #include <vector>
 #include <map>
 #include <memory>
+
 
 #include "RegularModels.h"
 #include "../test_helpers.h"
@@ -12,15 +14,19 @@ TEST_CASE("UniformMagneticField") {
     // DEFINITIONS
 
     // define positions in Galactic cartesian coordinates (units are kpc)
-    vector zeros{{0., 0., 0.}};
-    vector position{{-3.5, 2.6, 0.2}};
-    
+
+    double posx = -3.5;
+    double posy = 2.6;
+    double posz = 0.2;
+
     // define parameters to be updated
 
     double bx = -3.2;
     double by = 2.279;
     double bz = -0.1;
 
+    // define expected results of at_position
+    vector zeros{{0., 0., 0.}};
     vector updated{{bx, by, bz}};
 
     // define parameters for a regular grid in Galactic cartesian coordinates (units are kpc)
@@ -77,12 +83,12 @@ TEST_CASE("UniformMagneticField") {
         // constructor s
         UniformMagneticField umf_plain = UniformMagneticField();
         // test at_position -- default parameters
-        REQUIRE_THAT(umf_plain.at_position(zeros[0], zeros[1], zeros[2]), EqualsArray(zeros));
-        REQUIRE_THAT(umf_plain.at_position(position[0], position[1], position[2]), EqualsArray(zeros));
+        REQUIRE_THAT(umf_plain.at_position(0., 0., 0.), EqualsVector(zeros));
+        REQUIRE_THAT(umf_plain.at_position(posx, posy, posz), EqualsVector(zeros));
         // test on_grid -- default parameters
-        REQUIRE_THROWS(umf_plain.on_grid(), GridException);
-        REQUIRE_THAT(umf_plain.on_grid(shape, refpoint, increment), EqualsArray(default_regular_grid));
-        REQUIRE_THAT( umf_plain.on_grid(grid_x, grid_y, grid_z), EqualsArray(default_irregular_grid));
+        REQUIRE_THROWS_WITH(umf_plain.on_grid(), "The class has not been initialized with a grid, hence on_grid can only be called with a grid provided.");
+        REQUIRE_THAT(umf_plain.on_grid(shape, refpoint, increment), EqualsPointerArray(default_regular_grid, arr_sz));
+        REQUIRE_THAT(umf_plain.on_grid(grid_x, grid_y, grid_z), EqualsPointerArray(default_irregular_grid, 5));
         // parameter update
         umf_plain.bx = bx; 
         umf_plain.by = by;
@@ -91,15 +97,15 @@ TEST_CASE("UniformMagneticField") {
         REQUIRE(umf_plain.by == by); 
         REQUIRE(umf_plain.bz == bz); 
         // test at_position -- default parameters
-        REQUIRE_THAT(umf_plain.at_position(zeros[0], zeros[1], zeros[2]), EqualsArray(updated));
-        REQUIRE_THAT(umf_plain.at_position(position[0], position[1], position[2]), EqualsArray(updated));
+        REQUIRE_THAT(umf_plain.at_position(0., 0., 0.), EqualsVector(updated));
+        REQUIRE_THAT(umf_plain.at_position(posx, posy, posz), EqualsVector(updated));
         // test on_grid -- default parameters
-        REQUIRE_THROWS(umf_plain.on_grid(), GridException);
-        REQUIRE_THAT(umf_plain.on_grid(shape, refpoint, increment), EqualsArray(updated_regular_grid));
-        REQUIRE_THAT(umf_plain.on_grid(grid_x, grid_y, grid_z), EqualsArray(updated_irregular_grid));
+        REQUIRE_THROWS_WITH(umf_plain.on_grid(), "The class has not been initialized with a grid, hence on_grid can only be called with a grid provided.");
+        REQUIRE_THAT(umf_plain.on_grid(shape, refpoint, increment), EqualsPointerArray(updated_regular_grid, arr_sz));
+        REQUIRE_THAT(umf_plain.on_grid(grid_x, grid_y, grid_z), EqualsPointerArray(updated_irregular_grid, 5));
         #if autodiff_FOUND
         // test derivative 
-        REQUIRE_THAT(umf_plain.derivative(position[0], position[1], position[2]), EqualsArray(zeros));
+        REQUIRE_THAT(umf_plain.derivative(posx, posy, posz), EqualsVector(zeros));
         #endif
     }
 
@@ -107,12 +113,12 @@ TEST_CASE("UniformMagneticField") {
         // constructor s
         UniformMagneticField umf_regular_grid = UniformMagneticField(shape, refpoint, increment);
         // test at_position -- default parameters
-        REQUIRE_THAT(umf_regular_grid.at_position(zeros[0], zeros[1], zeros[2]), EqualsArray(zeros));
-        REQUIRE_THAT(umf_regular_grid.at_position(position[0], position[1], position[2]), EqualsArray(zeros));
+        REQUIRE_THAT(umf_regular_grid.at_position(0., 0., 0.), EqualsVector(zeros));
+        REQUIRE_THAT(umf_regular_grid.at_position(posx, posy, posz), EqualsVector(zeros));
         // test on_grid -- default parameters
-        REQUIRE_THAT(umf_regular_grid.on_grid(), EqualsArray(default_regular_grid));       
-        REQUIRE_THAT(umf_regular_grid.on_grid(shape, refpoint, increment), EqualsArray(default_regular_grid));
-        REQUIRE_THAT(umf_regular_grid.on_grid(grid_x, grid_y, grid_z), EqualsArray(default_irregular_grid));
+        REQUIRE_THAT(umf_regular_grid.on_grid(), EqualsPointerArray(default_regular_grid, arr_sz));       
+        REQUIRE_THAT(umf_regular_grid.on_grid(shape, refpoint, increment), EqualsPointerArray(default_regular_grid, arr_sz));
+        REQUIRE_THAT(umf_regular_grid.on_grid(grid_x, grid_y, grid_z), EqualsPointerArray(default_irregular_grid, 5));
         // parameter update
         umf_regular_grid.bx = bx; 
         umf_regular_grid.by = by;
@@ -121,15 +127,15 @@ TEST_CASE("UniformMagneticField") {
         REQUIRE(umf_regular_grid.by == by); 
         REQUIRE(umf_regular_grid.bz == bz); 
         // test at_position -- default parameters
-        REQUIRE_THAT(umf_regular_grid.at_position(zeros[0], zeros[1], zeros[2]), EqualsArray(updated));
-        REQUIRE_THAT(umf_regular_grid.at_position(position[0], position[1], position[2]), EqualsArray(updated));
+        REQUIRE_THAT(umf_regular_grid.at_position(0., 0., 0.), EqualsVector(updated));
+        REQUIRE_THAT(umf_regular_grid.at_position(posx, posy, posz), EqualsVector(updated));
         // test on_grid -- default parameters
-        REQUIRE_THAT(umf_regular_grid.on_grid(), EqualsArray(updated_regular_grid));       
-        REQUIRE_THAT(umf_regular_grid.on_grid(shape, refpoint, increment), EqualsArray(updated_regular_grid));
-        REQUIRE_THAT(umf_regular_grid.on_grid(grid_x, grid_y, grid_z), EqualsArray(updated_irregular_grid));
+        REQUIRE_THAT(umf_regular_grid.on_grid(), EqualsPointerArray(updated_regular_grid, arr_sz));       
+        REQUIRE_THAT(umf_regular_grid.on_grid(shape, refpoint, increment), EqualsPointerArray(updated_regular_grid, arr_sz));
+        REQUIRE_THAT(umf_regular_grid.on_grid(grid_x, grid_y, grid_z), EqualsPointerArray(updated_irregular_grid, 5));
         #if autodiff_FOUND
         // test derivative
-        REQUIRE_THAT(umf_regular_grid.derivative(position[0], position[1], position[2]), EqualsArray(zeros));
+        REQUIRE_THAT(umf_regular_grid.derivative(posx, posy, posz), EqualsVector(zeros));
         #endif
     }
 
@@ -137,12 +143,12 @@ TEST_CASE("UniformMagneticField") {
         // constructor s
         UniformMagneticField umf_irregular_grid = UniformMagneticField(grid_x, grid_y, grid_z);
         // test at_position -- default parameters
-        REQUIRE_THAT(umf_irregular_grid.at_position(zeros[0], zeros[1], zeros[2]), EqualsArray(zeros));
-        REQUIRE_THAT(umf_irregular_grid.at_position(position[0], position[1], position[2]), EqualsArray(zeros));
+        REQUIRE_THAT(umf_irregular_grid.at_position(0., 0., 0.), EqualsVector(zeros));
+        REQUIRE_THAT(umf_irregular_grid.at_position(posx, posy, posz), EqualsVector(zeros));
         // test on_grid -- default parameters
-        REQUIRE_THAT(umf_irregular_grid.on_grid(), EqualsArray(default_irregular_grid));       
-        REQUIRE_THAT(umf_irregular_grid.on_grid(shape, refpoint, increment), EqualsArray(default_regular_grid));
-        REQUIRE_THAT(umf_irregular_grid.on_grid(grid_x, grid_y, grid_z), EqualsArray(default_irregular_grid));
+        REQUIRE_THAT(umf_irregular_grid.on_grid(), EqualsPointerArray(default_irregular_grid, 5));       
+        REQUIRE_THAT(umf_irregular_grid.on_grid(shape, refpoint, increment), EqualsPointerArray(default_regular_grid, arr_sz));
+        REQUIRE_THAT(umf_irregular_grid.on_grid(grid_x, grid_y, grid_z), EqualsPointerArray(default_irregular_grid, 5));
         // parameter update
         umf_irregular_grid.bx = bx; 
         umf_irregular_grid.by = by;
@@ -151,15 +157,15 @@ TEST_CASE("UniformMagneticField") {
         REQUIRE(umf_irregular_grid.by == by); 
         REQUIRE(umf_irregular_grid.bz == bz); 
         // test at_position -- default parameters
-        REQUIRE_THAT(umf_irregular_grid.at_position(zeros[0], zeros[1], zeros[2]), EqualsArray(updated));
-        REQUIRE_THAT(umf_irregular_grid.at_position(position[0], position[1], position[2]), EqualsArray(updated));
+        REQUIRE_THAT(umf_irregular_grid.at_position(0., 0., 0.), EqualsVector(updated));
+        REQUIRE_THAT(umf_irregular_grid.at_position(posx, posy, posz), EqualsVector(updated));
         // test on_grid -- default parameters
-        REQUIRE_THAT(umf_irregular_grid.on_grid(), EqualsArray(updated_irregular_grid));       
-        REQUIRE_THAT(umf_irregular_grid.on_grid(shape, refpoint, increment), EqualsArray(updated_regular_grid));
-        REQUIRE_THAT(umf_irregular_grid.on_grid(grid_x, grid_y, grid_z), EqualsArray(updated_irregular_grid));
+        REQUIRE_THAT(umf_irregular_grid.on_grid(), EqualsPointerArray(updated_irregular_grid, 5));       
+        REQUIRE_THAT(umf_irregular_grid.on_grid(shape, refpoint, increment), EqualsPointerArray(updated_regular_grid, arr_sz));
+        REQUIRE_THAT(umf_irregular_grid.on_grid(grid_x, grid_y, grid_z), EqualsPointerArray(updated_irregular_grid, 5));
         #if autodiff_FOUND
         // test derivative
-        REQUIRE_THAT(umf_irregular_grid.derivative(position[0], position[1], position[2]), EqualsArray(zeros));
+        REQUIRE_THAT(umf_irregular_grid.derivative(posx, posy, posz), EqualsVector(zeros));
         #endif
     }
 }
--- c_library/test/test_helpers.h
+++ c_library/test/test_helpers.h
@@ -34,10 +34,85 @@ struct ArrayEqualsMatcher : Catch::Matchers::MatcherBase<std::array<T, N>> {
     const std::array<T, N>& m_expected;
 };
 
+template <typename T, size_t N>
+// old: struct ArrayEqualsMatcher : Catch::Matchers::Impl::MatcherBase<std::array<T, N>> {
+struct PointerArrayEqualsMatcher : Catch::Matchers::MatcherBase<std::array<T, N>> {
+    PointerArrayEqualsMatcher(const std::array<T, N>& expected, const size_t N_internal) : m_expected(expected), N2(N_internal) {}
+
+    bool match(const std::array<T, N>& arr) const override {
+        for (size_t i = 0; i < N; ++i) {
+            for (size_t j = 0; j < N2; ++j) {
+                if (arr[i][j] != m_expected[i][j]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    std::string describe() const override {
+        std::ostringstream oss;
+        oss << "equals: [";
+        for (size_t i = 0; i < N; ++i) {
+            if (i > 0) oss << "\n ";
+            for (size_t j = 0; j < N2; ++j) {
+                if (j > 0) oss << ", ";
+                oss << m_expected[i][j];
+            }
+        }
+        oss << "]";
+        return oss.str();
+    }
+
+    const std::array<T, N>& m_expected;
+    const size_t N2;
+};
+
+// old: struct ArrayEqualsMatcher : Catch::Matchers::Impl::MatcherBase<std::array<T, N>> {
+struct VectorEqualsMatcher : Catch::Matchers::MatcherGenericBase {
+    VectorEqualsMatcher(const vector& expected) : m_expected(expected) {}
+
+    size_t N = m_expected.size();
+
+    bool match(const vector& arr) const {
+        for (size_t i = 0; i < N; ++i) {
+            if (arr[i] != m_expected[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    std::string describe() const override {
+        std::ostringstream oss;
+        oss << "equals: [";
+        for (size_t i = 0; i < N; ++i) {
+            if (i > 0) oss << ", ";
+            oss << m_expected[i];
+        }
+        oss << "]";
+        return oss.str();
+    }
+
+    vector const& m_expected;
+};
+
+
 // Helper function to create the matcher
 template <typename T, size_t N>
 ArrayEqualsMatcher<T, N> EqualsArray(const std::array<T, N>& expected) {
     return ArrayEqualsMatcher<T, N>(expected);
+}
+
+// Helper function to create the matcher
+template <typename T, size_t N>
+PointerArrayEqualsMatcher<T, N> EqualsPointerArray(const std::array<T, N>& expected, const size_t N_internal) {
+    return PointerArrayEqualsMatcher<T, N>(expected, N_internal);
+}
+
+// Helper function to create the matcher
+auto EqualsVector(const vector& expected) -> VectorEqualsMatcher {
+    return VectorEqualsMatcher(expected);
 }
 
 
